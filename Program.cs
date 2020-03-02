152170d34393efdd3ed33cd2c0f8e58160f7763c
Eugene <61139973+eugeheless@users.noreply.github.com>
2020-03-02 17:40:43 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberQuiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введи число n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введи число m");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Random rnd = new Random();
+            Console.WriteLine("Попробуй угадай)))");
+            int w = Convert.ToInt32(Console.ReadLine());
+            int q = rnd.Next(n, m);
+            if (q == w)
+            {
+                Console.WriteLine("Красава");
+            }
+            else
+            {
+                Console.WriteLine("Не то");
+            }
+            Console.ReadKey();
+        }
+    }
+}
